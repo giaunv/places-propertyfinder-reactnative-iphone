bb3b82d89f766335523c5f1104b8938888b9ed8a
Giau Ngo <giaunv@gmail.com>
2015-03-28 18:38:30 +0700

    Hello React Native

--- PropertyFinderApp.js
+++ PropertyFinderApp.js
@@ -1,1 +1,21 @@
 // your code goes here
+'use strict';
+
+var React = require('react-native');
+
+var styles = React.StyleSheet.create({
+	text: {
+		color: 'black',
+		backgroundColor: 'white',
+		fontSize: 30,
+		margin: 80
+	}
+});
+
+class PropertyFinderApp extends React.Component {
+	render(){
+		return React.createElement(React.Text, {style: styles.text}, "Hello World");
+	}
+};
+
+React.AppRegistry.registerComponent('PropertyFinderApp', function(){ return PropertyFinderApp});
